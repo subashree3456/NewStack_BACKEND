fa98bb2f0c850250a389195d13b36a7025085b4b
SUBASHREE SANKARANARAYANAN <subashree3003@gmail.com>
2024-05-27 17:57:19 +0530

    updatedcorsoptionsdddddd

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ const corsOptions = {
   origin: '*',  // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'],  // Allow these headers
-  maxAge: 86400  // Cache preflight response for 1 day (86400 seconds)
+  maxAge: 86400 , // Cache preflight response for 1 day (86400 seconds),
+  "preflightContinue": false,
 };
 app.use(cors(corsOptions));
 const router = express.Router();
